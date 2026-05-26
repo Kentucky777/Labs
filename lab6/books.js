53dc0706b838a6d25dc05c740daeea75a23a9324
Никита <kentucky.twitch@mail.ru>
2026-05-26 23:24:20 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-Цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 380,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Фридрих Ницше',
+        name: 'Так говорил Заратустра',
+        publicationYear: 2024,
+        price: 490,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Фридрих Ницше',
+        name: 'По ту сторону добра и зла',
+        publicationYear: 2023,
+        price: 460,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Никколо Макиавелли',
+        name: 'Государь',
+        publicationYear: 2022,
+        price: 320,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Платон',
+        name: 'Государство',
+        publicationYear: 2021,
+        price: 550,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Луций Анней Сенека',
+        name: 'Нравственные письма к Луцилию',
+        publicationYear: 2023,
+        price: 410,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Жан Бодрийяр',
+        name: 'Симулякры и симуляция',
+        publicationYear: 2020,
+        price: 430,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Вольтер',
+        name: 'Кандид, или Оптимизм',
+        publicationYear: 2019,
+        price: 290,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Иммануил Кант',
+        name: 'Критика чистого разума',
+        publicationYear: 2024,
+        price: 620,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Марк Аврелий',
+        name: 'Наедине с собой. Размышления',
+        publicationYear: 2022,
+        price: 350,
+        bestseller: true,
+        inStock: true
+    },
+];
